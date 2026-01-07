c0afec3bd856506d733a5a8fd9302194d6d6ab63
Yana Kozlova <jasenkozlova@gmail.com>
2026-01-07 14:10:14 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import AddCompanyButton from '@/app/components/add-company-button';
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyButton />
+    </main>
+  );
+}
